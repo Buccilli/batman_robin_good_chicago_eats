a71a8bc1c3190291554f51b93368dbaa70bde79b
anonymous anonymous <alexandra.buccilli@kellogg.northwestern.edu>
2022-03-15 16:04:03 +0000

    Whitelist columns in Service for ActiveAdmin

--- app/admin/services.rb
+++ app/admin/services.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Service do
 
+ permit_params :server_name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
