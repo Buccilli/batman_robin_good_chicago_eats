579607825596408f9aab4aa1883e6dd0088c456f
anonymous anonymous <alexandra.buccilli@kellogg.northwestern.edu>
2022-03-15 16:04:00 +0000

    Whitelist columns in User for ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register User do
 
+ permit_params :liked_dishes, :desire_to_go_to_restaurant
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
