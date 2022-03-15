c0f2b328bdd4616c66f448886d6119185c448d79
anonymous anonymous <alexandra.buccilli@kellogg.northwestern.edu>
2022-03-15 16:04:33 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,12 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :liked_dishes, :string
+  attribute :desire_to_go_to_restaurant, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
