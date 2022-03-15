8e586bba12512f18e683339f48836acd8b9f781b
anonymous anonymous <alexandra.buccilli@kellogg.northwestern.edu>
2022-03-15 16:03:55 +0000

    Whitelist columns in Restaurant for ActiveAdmin

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Restaurant do
 
+ permit_params :good_service, :distance_to_joey, :price, :other_comments, :cuisine, :name, :neighborhood, :service_id, :added_by, :desire_to_go_to_restaurant
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
